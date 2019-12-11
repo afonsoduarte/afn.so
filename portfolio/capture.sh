9437fd897485d4a6a88f4c15e1d5df083e26aa45
afonsoduarte <afonsomartins@gmail.com>
2019-12-11 00:21:44 +0000

    Moves website to gatsby

--- portfolio/capture.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cat work-a.json | jq '.[] | .screenurl' --raw-output | webkit2png -C --clipwidth=1400 --clipheight=2000 -s 1 -D screens -
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="afn.so" />
+    <a href="mailto:a@afn.so">a@afn.so</a>
+  </Layout>
+)
+
+export default IndexPage
